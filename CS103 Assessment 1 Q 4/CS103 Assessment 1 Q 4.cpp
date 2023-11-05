5a544ec99e0e9331ea284c6672c738cd1a340f90
Matt T <mrsymtec@gmail.com>
2023-11-06 12:03:09 +1300

    Layed out where the functions are going to sit in the code

--- CS103 Assessment 1 Q 4/CS103 Assessment 1 Q 4.cpp
+++ CS103 Assessment 1 Q 4/CS103 Assessment 1 Q 4.cpp
@@ -3,9 +3,30 @@
 // This program allows the user to play hangman as outlined in the brief
 
 #include <iostream>
-#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
+// Function required to draw a line
+void drawLine(int length) {
+	for (int i = 0; i < length; i++) {
+		cout << "-";
+	}
+	cout << endl;
+}
+
+//Function to draw the Hangman
+
+
+// Function to draw a Walkman
+
+
+//Function to invert the Hangman
+
+
+// Function to play Hangman
 
 int main()
 {
